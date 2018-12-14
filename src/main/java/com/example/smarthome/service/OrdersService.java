144fe254ea6e7caa9d4fd6ad3e1aa25c3c6a8800
KathyLili <911606270@qq.com>
2018-12-14 21:56:16 +0800

    111

--- src/main/java/com/example/smarthome/service/OrdersService.java
+++ src/main/java/com/example/smarthome/service/OrdersService.java
@@ -6,13 +6,13 @@ import com.example.smarthome.vo.ResultBean;
 public interface OrdersService {
 
     //查询所有的订单信息
-    ResultBean selectAllOrders();
+    ResultBean selectAllOrders(Integer userId);
 
     //根据商品名称查询对应的订单信息
-    ResultBean selectOrders(String goodsname);
+    ResultBean selectOrders(String goodsname,Integer userId);
 
     //查询不同状态对应的订单信息
-    ResultBean selectOrdersByState(Integer state);
+    ResultBean selectOrdersByState(Integer state,Integer userId);
 
     //购物车商品付款之后生成订单信息
     ResultBean insertOrder(Orders orders);
